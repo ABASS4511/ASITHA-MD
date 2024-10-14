7bad01557830ed4446f197d2f7b0b8fc2963358d
ABASS4511 <sawadogoabdoul611@gmail.com>
2024-10-14 07:07:36 +0000

    Update config.js

--- config.js
+++ config.js
@@ -5,6 +5,6 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "𝙰𝚂𝙸𝚃𝙷𝙰-𝙼𝙳=ejxxwbZR#F38ZfesPcJ4QtpP4HXwmUjzMOJBAca_TtktiulSk3U0",
+SESSION_ID: process.env.SESSION_ID || "𝙰𝚂𝙸𝚃𝙷𝙰-𝙼𝙳=WyARxICL#hV63VImNlPOBTRvP6msrB6hI1JwYTkxWow2MyZF2A-M",
 MONGODB: process.env.MONGODB || "mongodb+srv://kulathungaasitha319:yjHB0DvFfStNfwPS@cluster0.3oijd.mongodb.net/",//enter mongo db url
 };
